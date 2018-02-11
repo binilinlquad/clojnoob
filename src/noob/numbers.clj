bb14f575ac4d57f0c543d3295c908e5438f7d57f
Chandra Yang <otama.ariq@gmail.com>
2018-02-11 21:49:44 +0700

    move numbers.clj into src folder

--- src/noob/numbers.clj
+++ src/noob/numbers.clj
@@ -1,4 +1,7 @@
-;; generate infinite even-numbers
+(ns noob.core
+  (:gen-class))
+
 (defn even-numbers
+  "generate infinite even-numbers"
   ([] (even-numbers 0))
   ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
